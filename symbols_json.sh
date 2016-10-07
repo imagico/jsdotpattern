17da71c8c16db1e1ad93fce6b723abab01ea8fb3
imagico <git@imagico.de>
2016-10-07 17:04:42 +0200

    add diagonal dash and new scrub symbols

--- symbols_json.sh
+++ symbols_json.sh
@@ -3,7 +3,7 @@
 #    assembles svg code from symbol files into json
 # ------------------------------------------------------------
 
-SYMBOLS="forest bforest scrub grass grass_tall dot dash dash_thin pixel vpixel tree_s1 tree_s2 tree_a1 tree_a2 tree_as1 tree_as2 tree tree2 mangrove trees1 trees2 trees3 trees4 trees5 tree_pair marsh reed bog scree rock wood wood_s wood1 wood2 wood2a quarry1 quarry2"
+SYMBOLS="forest bforest scrub scrub2 grass grass_tall dot dash dash_thin pixel vpixel tree_s1 tree_s2 tree_a1 tree_a2 tree_as1 tree_as2 tree tree2 mangrove trees1 trees2 trees3 trees4 trees5 tree_pair marsh reed bog scree rock wood wood_s wood1 wood2 wood2a quarry1 quarry2"
 
 for SYM in `find "symbols" -maxdepth 1 -name "*.svg" | sort` ; do
 	SYM_NAME=`basename $SYM .svg`
