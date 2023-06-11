d20f66f86fc36f7d6b16d892633fa98ed3b3b151
imagico <git@imagico.de>
2023-06-11 18:27:13 +0200

    updating pattern sanitizing script to new batch processing syntax of inkscape

--- svg_pattern.sh
+++ svg_pattern.sh
@@ -31,28 +31,20 @@ cp "$SRC" "$TARGET"
 # this is for normal jsdotpattern SVGs with <use>
 if grep "<use" "$TARGET" > /dev/null ; then
 	echo "processing non-inlined SVG..."
-	inkscape -g --verb EditSelectAll --verb SelectionUnGroup \
-			--verb EditSelectAll --verb EditUnlinkClone \
-			--verb EditSelectAll --verb SelectionUnGroup \
-			--verb EditSelectAll --verb SelectionUnGroup \
-			--verb EditSelectAll --verb ObjectToPath \
-			--verb EditSelectAll --verb SelectionCombine \
-			--verb FileSave --verb FileClose \
-			--verb FileQuit "$TARGET"
+	inkscape \
+		--actions="select-all:groups;object-release-clip;select-clear;select-by-element:rect;delete-selection;select-all;clone-unlink;select-all:groups;selection-ungroup;select-all:groups;selection-ungroup;select-all:groups;selection-ungroup;select-all;object-to-path;select-all;path-combine;select-all;object-release-clip;" \
+		--export-filename="$TARGET" --export-overwrite --export-type=SVG "$TARGET"
 
 else # this is for inline SVGs
 	# if clip is specified check for nested groups or transforms
 	# to see if this is necessary (or has been performed before)
 	if [ ! -z "$NOCLIP" ] || grep "</g></g></g>" "$TARGET" > /dev/null || grep "<g transform=" "$TARGET" > /dev/null ; then
 		echo "processing inlined SVG..."
-		inkscape -g --verb EditSelectAll --verb SelectionUnGroup \
-			--verb EditSelectAll --verb SelectionUnGroup \
-			--verb EditSelectAll --verb SelectionUnGroup \
-			--verb EditSelectAll --verb SelectionUnGroup \
-			--verb EditSelectAll --verb ObjectToPath \
-			--verb EditSelectAll --verb SelectionCombine \
-			--verb FileSave --verb FileClose \
-			--verb FileQuit "$TARGET"
+
+		inkscape \
+			--actions="select-all:groups;object-release-clip;select-clear;select-by-element:rect;delete-selection;select-all:groups;selection-ungroup;select-all:groups;selection-ungroup;select-all:groups;selection-ungroup;select-all;object-to-path;select-all;path-combine;select-all;object-release-clip;select-all:groups;selection-ungroup;" \
+			--export-filename="$TARGET" --export-overwrite --export-type=SVG "$TARGET"
+
 	else
 		echo "only clipping SVG since it seems to be sanitized already..."
 	fi
@@ -61,12 +53,12 @@ fi
 # this is pretty whacky since it depends on the specific structure of the file (specifically the presence of a <desc> tag at the very end)
 sed -i -e "s?<desc?<rect width=\"$SIZE\" height=\"$CLIP\" rx=\"0\" x=\"0\" y=\"0\" id=\"rect8566\" style=\"fill:none;fill-opacity:1;stroke:none\" /><desc?" "$TARGET"
 
-inkscape -g --verb EditSelectAll --verb SelectionIntersect \
-		--verb FileSave --verb FileClose \
-		--verb FileQuit "$TARGET"
+#-g --batch-process 
+inkscape \
+	--actions="select-all;path-intersection;" \
+	--export-filename="$TARGET" --export-overwrite --export-type=SVG "$TARGET"
 
 # and now we add this again as a workaround for Mapnik's senseless auto-centering
 sed -i -e "s?<desc?<rect width=\"$SIZE\" height=\"$CLIP\" rx=\"0\" x=\"0\" y=\"0\" id=\"rect8566\" style=\"fill:none;fill-opacity:1;stroke:none\" /><desc?" "$TARGET"
 
-inkscape -z --vacuum-defs -l "$TARGET" -f "$TARGET"
-
+inkscape --vacuum-defs --export-filename="$TARGET" --export-overwrite --export-type=SVG -l "$TARGET"
